4a2037ad29da8adf3ac3f45f4df650d2e9f7f3b7
Gabrielphnz <gabrielphnz@gmail.com>
2025-09-04 11:00:06 -0300

    Atualizar o MainActivity.kt

--- GerenciadorEstoqueFull/app/src/main/java/com/seuapp/estoque/MainActivity.kt
+++ GerenciadorEstoqueFull/app/src/main/java/com/seuapp/estoque/MainActivity.kt
@@ -3,6 +3,8 @@ package com.seuapp.estoque
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.seuapp.estoque.ui.AppScaffold
 import com.seuapp.estoque.ui.EstoqueViewModel
@@ -11,8 +13,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val vm: EstoqueViewModel = viewModel(factory = EstoqueViewModel.provideFactory(this))
-            AppScaffold(vm)
+            val vm: EstoqueViewModel = viewModel()
+            Surface(color = MaterialTheme.colorScheme.background) {
+                AppScaffold(vm)
+            }
         }
     }
 }
